b9cbf199100fb265169e07e3873f8dba0a9804f8
Chris Maness <christopher.maness@ncino.com>
2020-05-19 11:53:05 -0400

    Commiting cleaned up version of WoodChuckExecutor

--- R/WoodChuckExecutorFinal.R
+++ R/WoodChuckExecutorFinal.R
@@ -6,7 +6,15 @@ EstimateChuckingDistance <- function(wind_direction, temp, humidity, water_dista
     models <- PrepareModels()
     mylogit <- models$mylogit_model
 
-    age <- RetreiveAgeFromId()
+    if(wind_direction > 360 || wind_direction < 0){
+        stop("Variable wind_direction is out of bounds. Value Should be between 0 and 360")
+    }
+
+    if(humidity > 1 || humidity < 0){
+        stop("Variable humidity is out of bounds. Value should be between 0 and 1")
+    }
+
+    age <- RetreiveAgeFromId(chuck_id)
     
     newdata = data.frame(
         Wind.Direction=wind_direction, 
@@ -22,13 +30,16 @@ EstimateChuckingDistance <- function(wind_direction, temp, humidity, water_dista
 
 PrepareModels <- function(){
     mylogit <- readRDS(file = "./WoodChuckModel.R")
-    return list("mylogit_model" = mylogit)
+    preparedModels <- list("mylogit_model" = mylogit)
+    
+    return(preparedModels)
 }
 
-RetreiveAgeFromId <- function(){
+RetreiveAgeFromId <- function(chuck_id){
     db <- dbConnect(RSQLite::SQLite(), dbname="../Database/WoodChuckLookup.db")
     woodchuck <- dbGetQuery(db, paste("SELECT Age FROM WoodChucks where Id=", chuck_id))
-    return woodchuck[["Age"]]
+    
+    return(woodchuck[["Age"]])
 }
 
-EstimateChuckingDistance(wind_direction=124, temp=80, humidity=0.059, water_distance=99, chuck_id=1)
+EstimateChuckingDistance(wind_direction=360, temp=80, humidity=0.059, water_distance=99, chuck_id=1)
